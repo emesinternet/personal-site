dd662176c5e3edb9dc5ecb34970c16c1f1b6dbe2
Marc Singer <42449814+emesinternet@users.noreply.github.com>
2025-08-22 21:20:21 -0400

    Adjust background effect intensity and support color modes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, useColorModeValue } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
 import Home from "../Home";
 import WASaverPolicy from "../WASaverPolicy";
@@ -19,18 +19,20 @@ const Layout = () => {
     }
   };
 
+  const waveColor = useColorModeValue([0.9, 0.9, 0.9], [0.2, 0.2, 0.2]);
+
   return (
     <Box position="relative">
       <Box position="fixed" top={0} left={0} w="100%" h="100%" zIndex={-1}>
         <Dither
-          waveColor={[0.5, 0.5, 0.5]}
+          waveColor={waveColor}
           disableAnimation={false}
           enableMouseInteraction={true}
-          mouseRadius={0.3}
-          colorNum={4}
-          waveAmplitude={0.3}
-          waveFrequency={3}
-          waveSpeed={0.05}
+          mouseRadius={0.2}
+          colorNum={8}
+          waveAmplitude={0.15}
+          waveFrequency={2}
+          waveSpeed={0.02}
         />
       </Box>
       {renderContent()}
